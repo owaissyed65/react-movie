f16086c74e136c97a1b9ab03f1b4c031acd45dda
owaissyed65 <owaissyed65@gmail.com>
2023-06-03 17:53:34 +0500

    utils

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -6,7 +6,7 @@ import Select from "react-select";
 import "./styles.scss";
 
 import useFetch from "../../hooks/useFetch";
-import fetchDataFromApi from "../../utils/api";
+import fetchDataFromApi from "../../Utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
